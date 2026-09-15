58dee527056783661c008ced08f89994ed31bf70
sunayvenkat <sunayvenkat08@gmail.com>
2026-09-15 11:00:07 -0400

    Update portfolio.py

--- models/portfolio.py
+++ models/portfolio.py
@@ -1,1 +1,15 @@
 
+
+class portfolio:
+    def __init__(self, name, assets):
+        self.name = name
+        self.assets = assets
+
+    def total_value(self):
+        return sum(asset.value for asset in self.assets)
+
+    def add_asset(self, asset):
+        self.assets.append(asset)
+
+    def remove_asset(self, asset):
+        self.assets.remove(asset)
